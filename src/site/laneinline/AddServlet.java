f7d6a5f206f46ad9db07cd16c191ad77c5bca5b7
laneinline <44878400+laneinline@users.noreply.github.com>
2020-04-06 14:37:44 +0300

    addSession work

--- src/site/laneinline/AddServlet.java
+++ src/site/laneinline/AddServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet{
 	
@@ -19,8 +20,10 @@ public class AddServlet extends HttpServlet{
 		
 		int k = i +j;
 		
-
-		res.sendRedirect("sqr?k="+k); //URL Rewriting 
+		HttpSession addSession = req.getSession();
+		addSession.setAttribute("k", k);
+		
+		res.sendRedirect("sqr");
 		
 
 		
--- src/site/laneinline/SqrServlet.java
+++ src/site/laneinline/SqrServlet.java
@@ -6,13 +6,19 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SqrServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException  {
 		
-		int k = Integer.parseInt(req.getParameter("k"));
+		HttpSession sqrSession = req.getSession();
+		
+		int k = (int) sqrSession.getAttribute("k");
+		//int k = Integer.parseInt(req.getParameter("k"));
 		
 		k*=k;
+		
+		
 		//test for send redirect brunch
 		PrintWriter out = res.getWriter();
 		out.println("Resalt of adding and then sum^2 is: " + k);
