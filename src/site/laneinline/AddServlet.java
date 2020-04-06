60c374e0562ffd43d498935cf06fd87fa08e8a6c
laneinline <44878400+laneinline@users.noreply.github.com>
2020-04-06 10:43:39 +0300

    First commit

---
+++ src/site/laneinline/AddServlet.java
@@ -0,0 +1,30 @@
+package site.laneinline;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet{
+	
+	private static final long serialVersionUID = 1831973239L;
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i +j;
+		
+		//PrintWriter out = res.getWriter();
+		//out.println("Result is: " + k);
+		
+		req.setAttribute("k", k);
+		
+		RequestDispatcher rd = req.getRequestDispatcher("sqr");
+		rd.forward(req, res);
+	}
+}
