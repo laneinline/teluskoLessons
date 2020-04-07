08e99eac72d58539e53f942c5bafd1d64ef7b716
laneinline <44878400+laneinline@users.noreply.github.com>
2020-04-07 22:44:30 +0300

    Add annotations, removed servlet descriptions from web.xml

--- src/site/laneinline/AddServlet.java
+++ src/site/laneinline/AddServlet.java
@@ -5,11 +5,13 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet("/add")
 public class AddServlet extends HttpServlet{
 	
 	private static final long serialVersionUID = 1831973239L;
